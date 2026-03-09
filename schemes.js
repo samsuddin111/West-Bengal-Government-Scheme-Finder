2fda7dbc226c79bf81d311db85a884d398554865
Giveaway <skmd2482@gmail.com>
2026-03-09 19:06:06 +0530

    Create schemes.js with scheme details
    Added schemes array containing various schemes with details.

---
+++ schemes.js
@@ -0,0 +1,48 @@
+const schemes = [
+
+{
+name:"Lakshmir Bhandar",
+gender:"female",
+minAge:25,
+documents:[
+"Aadhar Card",
+"Bank Account",
+"Residence Certificate",
+"Ration Card"
+]
+},
+
+{
+name:"Kanyashree",
+gender:"female",
+minAge:13,
+maxAge:19,
+occupation:"student",
+documents:[
+"Aadhar Card",
+"School Certificate",
+"Bank Account"
+]
+},
+
+{
+name:"Krishak Bandhu",
+occupation:"farmer",
+documents:[
+"Aadhar Card",
+"Land Documents",
+"Bank Passbook"
+]
+},
+
+{
+name:"Swasthya Sathi",
+bpl:"yes",
+documents:[
+"Aadhar Card",
+"Ration Card",
+"Family Details"
+]
+}
+
+];
